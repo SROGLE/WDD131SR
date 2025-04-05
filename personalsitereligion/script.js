3b4142713ead6cf25f36dc60c2d57faae589055e
SROGLE <spencerghost09@gmail.com>
2025-04-05 17:21:17 -0600

    bom project done

---
+++ personalsitereligion/script.js
@@ -0,0 +1,34 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].className = "slide fade fade-out";
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].className = "slide fade";
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+// Fade in header on page load
+window.addEventListener('load', function() {
+    const header = document.querySelector('header');
+    header.classList.add('fade-in');
+});
