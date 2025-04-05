c68b9d7e455f7d0c98b14b23d13166bf5c603234
SROGLE <spencerghost09@gmail.com>
2025-04-05 17:40:23 -0600

    addd new changes

--- personalsitereligion/script.js
+++ personalsitereligion/script.js
@@ -5,30 +5,15 @@ function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("slide");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
-        slides[i].className = "slide fade fade-out";
         slides[i].style.display = "none";
+        slides[i].querySelector(".slide-content").classList.remove("active");
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].className = "slide fade";
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].querySelector(".slide-content").classList.add("active");
 }
-
-// Fade in header on page load
-window.addEventListener('load', function() {
-    const header = document.querySelector('header');
-    header.classList.add('fade-in');
-});
